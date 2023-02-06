f97e662a49d5667e3df3275aa42532ca6e88d5e6
Macosso <63609513+Macosso@users.noreply.github.com>
2023-02-06 22:04:51 +0100

    Topic Model and review categorization

--- src/SupportFunctions.R
+++ src/SupportFunctions.R
@@ -1,6 +1,7 @@
 library(tidytext)
 library(tm)
 library(topicmodels)
+library(scales)
 
 # write a function that takes a text column from a data frame and returns a plot of the most informative words for a given number of topics
 # function to get & plot the most informative terms by a specificed number
@@ -43,6 +44,69 @@ top_terms_by_topic_LDA <- function(input_text, # should be a columm from a dataf
     # if the user does not request a plot
     # return a list of sorted terms instead
     return(top_terms)
+  }
+}
+
+
+# given unsupervised topic modelling a go, let's try supervised topic modeling
+# use something call TF-IDF, which stands for "term frequency-inverse document frequency"
+# general idea
+# Words that are very common in a specific document are probably important to the topic of that document
+# words that are very common in all documents probably aren't important to the topics of any of them
+# a term will recieve a high weight if it's common in a specific document and also uncommon across all documents
+
+# some preprocessing
+# function that takes in a dataframe and the name of the columns
+# with the document texts and the topic labels. If plot is set to
+# false it will return the tf-idf output rather than a plot.
+top_terms_by_topic_tfidf <- function(text_df, text_column, group_column, plot = T){
+  # name for the column we're going to unnest_tokens_ to
+  # (you only need to worry about enquo stuff if you're
+  # writing a function using using tidyverse packages)
+  group_column <- enquo(group_column)
+  text_column <- enquo(text_column)
+  
+  # get the count of each word in each review
+  words <- text_df %>%
+    unnest_tokens(word, !!text_column) %>%
+    count(!!group_column, word) %>% 
+    ungroup()
+  
+  # get the number of words per text
+  total_words <- words %>% 
+    group_by(!!group_column) %>% 
+    summarize(total = sum(n))
+  
+  # combine the two dataframes we just made
+  words <- left_join(words, total_words)
+  
+  # get the tf_idf & order the words by degree of relevence
+  tf_idf <- words %>%
+    bind_tf_idf(word, !!group_column, n) %>%
+    select(-total) %>%
+    arrange(desc(tf_idf)) %>%
+    mutate(word = factor(word, levels = rev(unique(word))))
+  
+  if(plot == T){
+    # convert "group" into a quote of a name
+    # (this is due to funkiness with calling ggplot2
+    # in functions)
+    group_name <- quo_name(group_column)
+    
+    # plot the 10 most informative terms per topic
+    tf_idf %>% 
+      group_by(!!group_column) %>% 
+      top_n(10) %>% 
+      ungroup %>%
+      ggplot(aes(word, tf_idf*100, fill = as.factor(group_name))) +
+      geom_col(show.legend = FALSE) + 
+      labs(title = "Top words in each Rating", y = "Words", x = "Frequency") + 
+      scale_y_continuous(labels = scales::percent) +
+      facet_wrap(reformulate(group_name), scales = "free")+
+      coord_flip()
+  }else{
+    # return the entire tf_idf dataframe
+    return(tf_idf)
   }
 }
 
